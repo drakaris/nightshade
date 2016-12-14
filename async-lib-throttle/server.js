cf8adcf197bab51a267eb1628860b1ca286249fb
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-15 03:52:59 +0530

    Added feature - Resume scraping from CSV

--- async-lib-throttle/server.js
+++ async-lib-throttle/server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var async = require('async');
 var config = require('./config.json');
 var scraper = require('./modules/scraper');
@@ -20,7 +21,30 @@ global.scraperQueue.drain = function() {
   }
 };
 
-// Push root domain into urlStack
-global.urlStack.push(config.target);
-
-var scraperController = require('./modules/scraperController');
+// Check if url.csv exists
+if (fs.existsSync(config.csvFilename)) {
+  // Exists, load urls
+  require('csv-to-array')({
+    file: config.csvFilename,
+    columns: ['url']
+  }, function(err, array) {
+    // Push array data into completed stack
+    array.forEach(function(obj, key) {
+      if (key == array.length - 1) {
+        // Push data into urlStack
+        global.urlStack.push(obj.url);
+      } else {
+        // Push into completed
+        global.completed.push(obj.url);
+      }
+    });
+    // Continue scraping
+    var scraperController = require('./modules/scraperController');
+    var writeController = require('./modules/writeController');
+  });
+} else {
+  // Push root domain into urlStack
+  global.urlStack.push(config.target);
+  var scraperController = require('./modules/scraperController');
+  var writeController = require('./modules/writeController');
+}
