05e79eae1af0f13417a735764c939a12707004c8
Arjhun Srinivas <arjhun.s@gmail.com>
2016-12-15 02:48:16 +0530

    Added feature - Resume scraping from CSV

--- server.js
+++ server.js
@@ -17,14 +17,45 @@ global.writerStack = [];
 
 global.urlStack.push(config.target);
 
-// Make initial scrape request
-scraper(global.urlStack.shift(), function(err, url) {
-  if (err) throw err;
-  // URL complete, push to completed stack
-  global.completed.push(url);
-  // Start while loop controller
-  // console.log('URL count: ' + global.urlStack.length);
-  // Require auto controller
-  var scraperController = require('./modules/scraperController');
-  var writeController = require('./modules/writeController');
-});
+// Check if url.csv exists
+if (fs.existsSync(config.csvFilename)) {
+  // Exists, load urls
+  require('csv-to-array')({
+    file: config.csvFilename,
+    columns: ['url']
+  }, function(err, array) {
+    // Push array data into completed stack
+    array.forEach(function(obj, key) {
+      if (key == array.length - 1) {
+        // Push data into urlStack
+        global.urlStack.push(obj.url);
+      } else {
+        // Push into completed
+        global.completed.push(obj.url);
+      }
+    });
+    // Continue scraping
+    scraper(global.urlStack.shift(), function(err, url) {
+      if (err) throw err;
+      // URL complete, push to completed stack
+      global.completed.push(url);
+      // Start while loop controller
+      // console.log('URL count: ' + global.urlStack.length);
+      // Require auto controller
+      var scraperController = require('./modules/scraperController');
+      var writeController = require('./modules/writeController');
+    });
+  });
+} else {
+  // Make initial scrape request
+  scraper(global.urlStack.shift(), function(err, url) {
+    if (err) throw err;
+    // URL complete, push to completed stack
+    global.completed.push(url);
+    // Start while loop controller
+    // console.log('URL count: ' + global.urlStack.length);
+    // Require auto controller
+    var scraperController = require('./modules/scraperController');
+    var writeController = require('./modules/writeController');
+  });
+}
